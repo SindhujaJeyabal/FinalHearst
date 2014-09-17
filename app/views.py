671d99b84c02cf63cdf7a828b7b3e5a9ce610fd5
AdityaMishra <aditya.mishra@ischool.berkeley.edu>
2014-09-16 18:25:34 -0700

    changed static css paths

--- app/views.py
+++ app/views.py
@@ -1,6 +1,16 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
+@app.route('/homepage.html')
+
+def main():
+	return render_template('homepage.html', name="name")
+
 @app.route('/index')
 def index():
-    return "Hello, World!"
+	return "Hello, World!"
+
+@app.route('/satchel.html')
+def satrender():
+	return render_template('satchel.html', name="name")
